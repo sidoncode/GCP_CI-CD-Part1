d8fc5ed79ed8410af0c25585b0c4dd9632d5ae0f
Raghav Arora <raarora09@gmail.com>
2024-12-22 12:33:45 +0530

    first commit

--- www/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('timeButton').addEventListener('click', function() {
-    const currentTime = new Date();
-    document.getElementById('currentTime').innerText = `The current local time is: ${currentTime.toLocaleTimeString()}`;
-});
